129c71e16daf5704664d8265039b7410c6aff490
Radhitia Pratama <radhitia43@gmail.com>
2023-07-14 10:51:39 +0700

    fix responsive navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,35 @@
-document.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
-});
+// document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+// });
 
-document.addEventListener("keydown", function (e) {
-    if (
-        e.ctrlKey &&
-        e.keyCode == 67 ||
-        e.keyCode == 85 ||
-        e.keyCode == 86 ||
-        e.keyCode == 117 ||
-        e.keyCode == 123
+// document.addEventListener("keydown", function (e) {
+//     if (
+//         e.ctrlKey &&
+//         e.keyCode == 67 ||
+//         e.keyCode == 85 ||
+//         e.keyCode == 86 ||
+//         e.keyCode == 117 ||
+//         e.keyCode == 123
 
-    ) {
-        e.preventDefault();
-    }
-});
+//     ) {
+//         e.preventDefault();
+//     }
+// });
 
+// disabled draggable image
 const images = document.querySelectorAll("img");
 images.forEach(function (img) {
     img.setAttribute('draggable', false);
 })
+
+const hamburgerMenu = document.querySelector("nav .hamburger-menu");
+const navItems = document.querySelector("nav .nav-items");
+
+hamburgerMenu.addEventListener("click", function () {
+    const active = document.querySelector(".nav-items.active");
+    if (active) {
+        active.classList.remove("active");
+        return;
+    }
+    navItems.classList.add("active");
+});
